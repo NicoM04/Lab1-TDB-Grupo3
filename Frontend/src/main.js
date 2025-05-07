e1bc03b764e06eab03d2651a4e293b94e81e4b2a
Thomas Gustafsson <thomas.gustafsson@usach.cl>
2025-05-07 17:44:36 -0400

    iniciando el frontend

---
+++ Frontend/src/main.js
@@ -0,0 +1,32 @@
+/*import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router/index.js'; 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+const app = createApp(App);
+
+
+
+app.use(router);
+app.use(PrimeVue, {
+	theme: {
+			preset: Aura,
+			options: {
+					prefix: 'p',
+					darkModeSelector: 'system',
+					cssLayer: false
+			}
+	}
+});
+app.mount('#app');
+*/
+
+
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
